49c233453eae89ab14f9097eb9ac63d43fe8a0e4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-07 23:26:25 -0400

    spelling: nonexistent
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/main/java/io/valkey/Jedis.java
+++ src/main/java/io/valkey/Jedis.java
@@ -1631,8 +1631,8 @@ public class Jedis implements ServerCommands, DatabaseCommands, JedisCommands, J
    * go from head to tail that is the normal behaviour. So for example LREM with count -2 and hello
    * as value to remove against the list (a,b,c,hello,x,hello,hello) will leave the list
    * (a,b,c,hello,x). The number of removed elements is returned as an integer, see below for more
-   * information about the returned value. Note that non existing keys are considered like empty
-   * lists by LREM, so LREM against non existing keys will always return 0.
+   * information about the returned value. Note that nonexistent keys are considered like empty
+   * lists by LREM, so LREM against nonexistent keys will always return 0.
    * <p>
    * Time complexity: O(N) (with N being the length of the list)
    * @param key
@@ -6078,8 +6078,8 @@ public class Jedis implements ServerCommands, DatabaseCommands, JedisCommands, J
    * go from head to tail that is the normal behaviour. So for example LREM with count -2 and hello
    * as value to remove against the list (a,b,c,hello,x,hello,hello) will leave the list
    * (a,b,c,hello,x). The number of removed elements is returned as an integer, see below for more
-   * information about the returned value. Note that non existing keys are considered like empty
-   * lists by LREM, so LREM against non existing keys will always return 0.
+   * information about the returned value. Note that nonexistent keys are considered like empty
+   * lists by LREM, so LREM against nonexistent keys will always return 0.
    * <p>
    * Time complexity: O(N) (with N being the length of the list)
    * @param key
